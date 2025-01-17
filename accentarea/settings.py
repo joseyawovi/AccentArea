7fbb64c1c4e2ccfcf8e726a3a1d8f5fc13b91e76
joseyawovi Yawovi <joseyawovi@gmail.com>
2025-01-17 20:19:33 +0000

    Remove .env file from version control

--- accentarea/settings.py
+++ accentarea/settings.py
@@ -108,6 +108,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
