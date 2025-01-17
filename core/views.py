ef8f59bab605bbe8fc0571885916b30147d45d7e
joseyawovi Yawovi <joseyawovi@gmail.com>
2025-01-17 17:25:39 +0000

    static end, starting settings

--- core/views.py
+++ core/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.https import HttpResponse
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello world")
-
-# echo "# AccentArea" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/joseyawovi/AccentArea.git
-# git push -u origin main
-
