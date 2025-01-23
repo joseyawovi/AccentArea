aa24e019e18036a84acb62e84c97ea0a96e22b11
joseyawovi Yawovi <joseyawovi@gmail.com>
2025-01-23 07:14:50 +0000

    ready to render

--- accentarea/settings.py
+++ accentarea/settings.py
@@ -111,9 +111,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 if not DEBUG:    
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
